36a324c4086d977f649934812c6f42cee3a9349d
Turdiali <xasanbayevturdiali92@gmail.com>
2026-03-06 19:24:54 +0500

    Admin register writing

--- todo/admin.py
+++ todo/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin
+
 from .models import CustomUser, Profile
+
 
 admin.site.site_header = "ToDo Admin Dashboard"
 admin.site.site_title = "ToDo Admin Dashboard"
@@ -8,5 +11,24 @@ admin.site.index_title = "Welcome to ToDo Admin Dashboard"
 admin.site.empty_value_display = "None"
 
 
-admin.site.register(CustomUser)
-admin.site.register(Profile)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("email", "is_staff")
+    filter_horizontal = ("groups", "user_permissions")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {"classes": ("wide"), "fields": ("email", "usable_password", "password1", "password2")})
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('full_name',)
+    search_fields = ('full_name',)
