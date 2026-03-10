470205ae5de4c70836f3dea135e72cf7be650fd3
Turdiali <xasanbayevturdiali92@gmail.com>
2026-03-10 21:18:16 +0500

    Setup

--- todo/admin.py
+++ todo/admin.py
@@ -7,7 +7,7 @@ from .models import CustomUser, Profile, ToDo
 admin.site.site_header = "ToDo Admin Dashboard"
 admin.site.site_title = "ToDo Admin Dashboard"
 admin.site.index_title = "Welcome to ToDo Admin Dashboard"
-admin.site.empty_value_display = "Nonea"
+admin.site.empty_value_display = "None"
 
 
 @admin.register(CustomUser)
